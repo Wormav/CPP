e38f84d2005dcc22a4b639483ff200b83a386260
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-01-12 16:36:22 +0100

    cpp 00 ex01 try

--- CPP00/ex01/phone_book.cpp
+++ CPP00/ex01/phone_book.cpp
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 12:01:33 by jlorette          #+#    #+#             */
-/*   Updated: 2025/01/12 15:48:25 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/01/12 16:34:03 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,13 @@ int main()
 		std::cout << "Enter command (ADD, SEARCH, EXIT): ";
 		if (!std::getline(std::cin, command))
 			break;
-
-		if (command == "ADD")
+		if (command == "ADD" || command == "add")
 			phoneBook.addContact();
-		else if (command == "SEARCH")
+		else if (command == "SEARCH" || command == "search")
 			phoneBook.searchContact();
-		else if (command == "EXIT")
+		else if (command == "EXIT" || command == "exit")
 			break;
 	}
 
-	return 0;
+	return (0);
 }
